b7c8e76d8fe4b9fbc78af97bb5e566bd0030481f
Zalipuhin-va <zalipuhin.va@gmail.com>
2019-06-08 08:40:22 +0900

    New

--- src/ru/chita/gres/Truck_Catalog/objects/Truck.java
+++ src/ru/chita/gres/Truck_Catalog/objects/Truck.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Truck {
     Scanner in = new Scanner(System.in);
 
-    String name_truck;
-    long vin;
+     String name_truck;
+     long vin;
 
     public void GetTruck(){
         System.out.println("Введите Name: ");
