4d4f5cdffa7f5846e38bd4e75606d6917bd4b540
Zalipuhin-va <zalipuhin.va@gmail.com>
2019-06-07 23:49:18 +0900

    Продолжение

--- src/ru/chita/gres/Truck_Catalog/Main.java
+++ src/ru/chita/gres/Truck_Catalog/Main.java
@@ -1,8 +1,15 @@
 package ru.chita.gres.Truck_Catalog;
 
+import ru.chita.gres.Truck_Catalog.objects.Truck;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    
+        Scanner in = new Scanner(System.in);
+        Truck truck = new Truck();
+
+        
     }
 }
--- src/ru/chita/gres/Truck_Catalog/objects/Truck.java
+++ src/ru/chita/gres/Truck_Catalog/objects/Truck.java
@@ -1,4 +1,19 @@
 package ru.chita.gres.Truck_Catalog.objects;
 
+import java.util.Scanner;
+
 public class Truck {
+    Scanner in = new Scanner(System.in);
+
+    String name_truck;
+    long vin;
+
+    public String getName_truck() {
+        return name_truck;
+    }
+
+    public long getVin() {
+        return vin;
+    }
+
 }
